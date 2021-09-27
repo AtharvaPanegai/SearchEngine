75308d0a181f36f51ab3f5faf16bf394a99942d5
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-27 23:54:44 +0530

    transfering data from data layer to the searchResult Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Router>
         <Switch>
           <Route path='/search'>
-            <h1>This is Search Page</h1>
+            <SearchPage />
           </Route>
           <Route path='/'>
             <Home />
