fcb584947d3bd041317b9ffe4df448abe1c7162f
AtharvaPanegai <panegai.atharv@gmail.com>
2021-09-27 20:37:31 +0530

    Search Page

--- src/components/search.js
+++ src/components/search.js
@@ -1,18 +1,46 @@
-import React from 'react'
-import "./search.css"
+/** @format */
+
+import React, { useState } from "react";
+import "./Search.css";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsMicFill } from "react-icons/bs";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
-function search() {
-    return (
-        <div className = "search">
-            <div className = "searchInput">
-                <AiOutlineSearch />
-                <input />
-                <BsMicFill />
-            </div>
+function Search({ hideButtons = false }) {
+  const [input, setInput] = useState("");
+  const history = useHistory();
+
+  const searchFunction = (e) => {
+    e.preventDefault();
+    console.log("You hit the search Button");
+    history.push("/search");
+  };
+
+  return (
+    <form className='search'>
+      <div className='searchInput'>
+        <AiOutlineSearch className='searchInputIcon' />
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <BsMicFill />
+      </div>
+      {!hideButtons ? (
+        <div className='searchButtons'>
+          <Button type='submit' onClick={searchFunction} variant='outline-dark'>
+            Google Search
+          </Button>
+          <Button variant='outline-dark'>I'm Feeling Lucky</Button>
         </div>
-    )
+      ) : (
+        <div className='searchButtons'>
+          <Button className = "buttonsHidden" type='submit' onClick={searchFunction} variant='outline-dark'>
+            Google Search
+          </Button>
+          <Button className = "buttonsHidden" variant='outline-dark'>I'm Feeling Lucky</Button>
+        </div>
+      )}
+    </form>
+  );
 }
 
-export default search
+export default Search;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 // icons
-import { AiOutlineAppstore } from "react-icons/ai";
+// import { AiOutlineAppstore } from "react-icons/ai";
 import { FaUserAlt } from "react-icons/fa";
-import Search from "../components/search";
+import Search from "../components/Search";
 
 function Home() {
   return (
@@ -25,7 +25,7 @@ function Home() {
       </div>
       <div className='homeBody'>
         <img
-          src='https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg'
+          src='https://prescribedhealth.in/wp-content/uploads/2021/09/Search-1.png'
           alt='Search.com'
         />
         <div className = "homeInputContainer">
